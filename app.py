b0ec3fc9adce3d689a7446ae49208e0d1e18717e
muthiafaiqa <mnurfaiqa@gmail.com>
2025-12-14 22:59:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,28 @@ import numpy as np
 
 st.title("Klasifikasi Transaksi")
 
+# Load model dan scaler
 model, scaler = joblib.load("model_rf_classification.pkl")
 
+# Input user
 total = st.number_input("Total Harga (Rp)", min_value=0)
 qty = st.number_input("Kuantitas", min_value=1)
 
 if st.button("Prediksi"):
+    # Transform input
     data = scaler.transform([[total, qty]])
-    pred = model.predict(data)[0]
+    
+    # Prediksi cluster
+    cluster = model.predict(data)[0]
 
+    # Mapping cluster ke label
     cluster_label = {
-    0: "Transaksi Kecil",
-    2: "Transaksi Sedang",
-    1: "Transaksi Besar"
-}
+        0: "Transaksi Kecil",
+        1: "Transaksi Besar",
+        2: "Transaksi Sedang"
+    }
 
-hasil = cluster_label[cluster]
+    # Ambil label
+    hasil = cluster_label[cluster]
 
-
-st.success(f"Hasil Klasifikasi: {label[pred]}")
+    st.success(f"Hasil Prediksi: {hasil}")
